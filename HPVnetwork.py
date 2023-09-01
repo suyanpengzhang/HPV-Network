a8524c6a9bf5ab83e41c3fb7d20f34882ccce302
suyanpengzhang <suyanpengzhang@gmail.com>
2023-09-01 09:17:53 -0700

    update

--- HPVnetwork.py
+++ HPVnetwork.py
@@ -30,13 +30,26 @@ class HPV_network(object):
             print('Error: data & nodes mismatch')
             return
         for node in range(len(attr_table)):
-            for atr in range(1,len(attr_table.columns)):
+            for atr in range(0,len(attr_table.columns)):
                 self.G.nodes[node][attr_table.columns[atr]] = attr_table[attr_table.columns[atr]][node]
+    def add_colors(self):
+        for node in range(len(self.G.nodes)):
+            if self.G.nodes[node]['initial attitude']<12:
+                self.G.nodes[node]['color'] = 'red'
+            elif self.G.nodes[node]['initial attitude']>=24:   
+                self.G.nodes[node]['color'] = 'blue'
+            else:
+                self.G.nodes[node]['color'] = 'green'
     def generate_di_graph(self,rand):
         if rand == 1:
             NewG = nx.DiGraph(self.G)
             for edge in NewG.edges:
-                NewG[edge[0]][edge[1]]['weight'] = 0.1
+                if NewG.nodes[edge[0]]['initial attitude'] < 12:
+                    NewG[edge[0]][edge[1]]['weight'] = 0.5
+                elif NewG.nodes[edge[0]]['initial attitude'] >= 24:
+                    NewG[edge[0]][edge[1]]['weight'] = 0.4
+                else:
+                    NewG[edge[0]][edge[1]]['weight'] = 0.55
             self.G = NewG
 # =============================================================================
 #         nx.draw(self.G, with_labels=True)
@@ -49,19 +62,31 @@ class HPV_network(object):
     def run_linear_threshold_model(self,rand,threshold_pos,threshold_neg,inital_pos,inital_neg, num_initial_pos,num_initial_neg,time_periods):
         if rand == 1:
             self.LTM = []
-            inital = random.sample(list(self.G.nodes),k=num_initial_pos+num_initial_neg)
-            #print(inital)
-            if inital_pos == []:
-                inital_pos = inital[:num_initial_pos]
-            if inital_neg == []:
-                inital_neg = inital[num_initial_pos:num_initial_pos+num_initial_neg]
+# =============================================================================
+#             inital = random.sample(list(self.G.nodes),k=num_initial_pos+num_initial_neg)
+#             #print(inital)
+#             if inital_pos == []:
+#                 inital_pos = inital[:num_initial_pos]
+#             if inital_neg == []:
+#                 inital_neg = inital[num_initial_pos:num_initial_pos+num_initial_neg]
+#             for node in range(len(self.G.nodes)):
+#                 if node in inital_pos:
+#                     self.G.nodes[node]['status'] = 1
+#                 elif node in inital_neg:
+#                     self.G.nodes[node]['status'] = -1
+#                 else:
+#                     self.G.nodes[node]['status'] = 0
+# =============================================================================
             for node in range(len(self.G.nodes)):
-                if node in inital_pos:
+                if self.G.nodes[node]['initial attitude']<12:
                     self.G.nodes[node]['status'] = 1
-                elif node in inital_neg:
+                    self.G.nodes[node]['color'] = 'red'
+                elif self.G.nodes[node]['initial attitude']>=24:   
                     self.G.nodes[node]['status'] = -1
+                    self.G.nodes[node]['color'] = 'blue'
                 else:
                     self.G.nodes[node]['status'] = 0
+                    self.G.nodes[node]['color'] = 'green'
             self.LTM.append(self.G)
             if threshold_pos == None:
                 threshold_pos = np.random.random()
@@ -102,18 +127,26 @@ class HPV_network(object):
                         #print(newG.nodes.data('status'))
                 for node in pos:
                     newG.nodes[node]['status'] = 1
+                    newG.nodes[node]['color'] = 'red'
                 for node in neg:
                     newG.nodes[node]['status'] = -1     
+                    newG.nodes[node]['color'] = 'blue'
                 copy_G = newG.copy()
                 self.LTM.append(copy_G)
 population = 100
 X = HPV_network(population, 7, None)
 X.generate_BAGraph()
-data = {'id': [np.random.randint(100000) for i in range(population)], 'age': [np.random.randint(100) for i in range(population)], 'gender': [np.random.randint(1) for i in range(population)]}
+data_neg = [np.random.randint(24,36) for i in range(4)]
+data_neu = [np.random.randint(12,24) for i in range(51)]
+data_pos = [np.random.randint(0,12) for i in range(45)]
+atttudes = data_neg+data_neu+data_pos
+random.shuffle(atttudes)
+data = { 'initial attitude': atttudes}
 df = pd.DataFrame(data=data)
 X.add_attributes_to_nodes(df)
+X.add_colors()
 X.generate_di_graph(rand=1)
-X.run_linear_threshold_model(rand = 1, threshold_pos=0.1,threshold_neg=-0.1,inital_pos=[],inital_neg=[],num_initial_pos=1,num_initial_neg=1,time_periods=5)
+X.run_linear_threshold_model(rand = 1, threshold_pos=4,threshold_neg=-0.45,inital_pos=[],inital_neg=[],num_initial_pos=1,num_initial_neg=1,time_periods=5)
 #print(X.G.nodes.data())
 t=0
 for Gs in X.LTM:
@@ -125,29 +158,19 @@ for Gs in X.LTM:
     means = np.mean(ls)
     mask_pos = np.where(ls==1)
     print(means)
-    age = np.array([Gs.nodes.data('age')[i] for i in Gs.nodes])
+    age = np.array([Gs.nodes.data('initial attitude')[i] for i in Gs.nodes])
     pos_age = age[mask_pos]
     meansage = np.mean(pos_age)
     print('Num pos',len(mask_pos[0]))
-    print('Mean age pos',meansage)
+    print('Mean initial att',meansage)
     print('Node 0 Att',Gs.nodes.data('status')[0])
+    node_colors = [Gs.nodes.data('color')[i] for i in range(population)]
+    nx.draw_networkx(Gs, with_labels=True, node_color=node_colors)
+    plt.show()
 #print(X.G.edges.data())
 
 def calculate_assortativity(graph, attribute):
     return nx.attribute_assortativity_coefficient(graph, attribute)
-G = nx.Graph()
-ages = [24, 24, 24, 34, 35, 35, 36, 46]
-for i in range(8):
-    G.add_node(i)
-for i in range(8):
-    G.nodes[i]['age'] = ages[i]
-age_threshold = 5
 
-for node1 in G.nodes():
-    for node2 in G.nodes():
-        if node1 != node2 and abs(G.nodes[node1]['age'] - G.nodes[node2]['age']) <= age_threshold:
-            G.add_edge(node1, node2)
-nx.draw_networkx(G, with_labels=True)
-plt.show()
-assortativity = calculate_assortativity(G, 'age')
+assortativity = calculate_assortativity(Gs, 'age')
 print("Assortativity:", assortativity)
